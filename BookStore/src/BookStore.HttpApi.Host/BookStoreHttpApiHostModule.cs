8131f342d8adf28781774fa20654ed422a8d5955
ekapituro <ekapituro@gmail.com>
2024-06-22 07:43:54 +0200

    Update HTTPS metadata requirement for authentication
    The appsettings.json and BookStoreHttpApiHostModule.cs files have been updated to enforce HTTPS metadata validation for authentication. This change enhances the security of the authentication process. The unused OpenIddictServerOptions configuration has also been removed for code clarity.

--- BookStore/src/BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ BookStore/src/BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -75,17 +75,11 @@ public class BookStoreHttpApiHostModule : AbpModule
         ConfigureSwaggerServices(context, configuration);
 
 
-        // context.Services.Configure<OpenIddictServerOptions>(OpenIddictServerDefaults.ConfigurationKey, configuration =>
-        // {
-        //     configuration.UseAspNetCore();
-        //     configuration.SetTokenEndpointUris("/connect/token");
-        // });
-
         context.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
            .AddJwtBearer(options =>
             {
                 options.Authority = configuration["AuthServer:Authority"];
-               // options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
+                options.RequireHttpsMetadata = Convert.ToBoolean(configuration["AuthServer:RequireHttpsMetadata"]);
                 options.Audience = "BookStoreMaui"; // Replace with your actual audience
                 options.TokenValidationParameters.ValidIssuers = configuration.GetSection("AuthServer:ValidIssuers").Get<string[]>();
             });
