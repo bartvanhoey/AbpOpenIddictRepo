f5be405700cfed9a097a70d11283014db686f416
bartvanhoey <bartvanhoey@hotmail.com>
2024-06-27 14:34:19 +0200

    Update

---
+++ BookStoreMaui/BookStoreMaui/Services/OpenIddict/Infra/ConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace BookStoreMaui.Services.OpenIddict.Infra
+{
+    public static class ConfigurationExtensions
+    {
+        public static OpenIddictSettings GetOidcSettings(this IConfiguration configuration)
+        {
+            var oIddict = configuration.GetSection(nameof(OpenIddictSettings)).Get<OpenIddictSettings>();
+            if (oIddict == null) throw new ArgumentNullException(nameof(OpenIddictSettings));
+            return oIddict;
+        }
+    }
+}
