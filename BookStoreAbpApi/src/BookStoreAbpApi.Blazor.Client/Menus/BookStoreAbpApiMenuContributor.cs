b09ce5c49993b9ee24ee15073131ce7889471e08
ekapituro <ekapituro@gmail.com>
2024-07-07 09:18:21 +0200

    Update

--- BookStoreAbpApi/src/BookStoreAbpApi.Blazor.Client/Menus/BookStoreAbpApiMenuContributor.cs
+++ BookStoreAbpApi/src/BookStoreAbpApi.Blazor.Client/Menus/BookStoreAbpApiMenuContributor.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using BookStore.Localization;
-using BookStore.MultiTenancy;
+using BookStoreAbpApi.Localization;
+using BookStoreAbpApi.MultiTenancy;
 using Volo.Abp.Account.Localization;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Identity.Blazor;
 using Volo.Abp.SettingManagement.Blazor.Menus;
 using Volo.Abp.TenantManagement.Blazor.Navigation;
 using Volo.Abp.UI.Navigation;
+using BookStoreAbpApi.Permissions;
 
-namespace BookStore.Blazor.Client.Menus;
+namespace BookStoreAbpApi.Blazor.Client.Menus;
 
-public class BookStoreMenuContributor : IMenuContributor
+public class BookStoreAbpApiMenuContributor : IMenuContributor
 {
     private readonly IConfiguration _configuration;
 
-    public BookStoreMenuContributor(IConfiguration configuration)
+    public BookStoreAbpApiMenuContributor(IConfiguration configuration)
     {
         _configuration = configuration;
     }
@@ -33,14 +34,14 @@ public class BookStoreMenuContributor : IMenuContributor
         }
     }
 
-    private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    private async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
     {
-        var l = context.GetLocalizer<BookStoreResource>();
+        var l = context.GetLocalizer<BookStoreAbpApiResource>();
 
         context.Menu.Items.Insert(
             0,
             new ApplicationMenuItem(
-                BookStoreMenus.Home,
+                BookStoreAbpApiMenus.Home,
                 l["Menu:Home"],
                 "/",
                 icon: "fas fa-home"
@@ -61,22 +62,34 @@ public class BookStoreMenuContributor : IMenuContributor
         administration.SetSubItemOrder(IdentityMenuNames.GroupName, 2);
         administration.SetSubItemOrder(SettingManagementMenus.GroupName, 3);
 
-        context.Menu.AddItem(
-    new ApplicationMenuItem(
-        "BooksStore",
-        l["Menu:BookStore"],
-        icon: "fa fa-book"
-    ).AddItem(
-        new ApplicationMenuItem(
-            "BooksStore.Books",
-            l["Menu:Books"],
-            url: "/books"
-        )
-    )
-);
+        var bookStoreMenu = new ApplicationMenuItem(
+            "BooksStore",
+            l["Menu:BookStore"],
+            icon: "fa fa-book"
+        );
 
+        context.Menu.AddItem(bookStoreMenu);
 
-        return Task.CompletedTask;
+        //CHECK the PERMISSION
+        if (await context.IsGrantedAsync(BookStoreAbpApiPermissions.Books.Default))
+        {
+            bookStoreMenu.AddItem(new ApplicationMenuItem(
+                "BooksStore.Books",
+                l["Menu:Books"],
+                url: "/books"
+            ));
+        }
+        
+        
+        if (await context.IsGrantedAsync(BookStoreAbpApiPermissions.Authors.Default))
+        {
+            bookStoreMenu.AddItem(new ApplicationMenuItem(
+                "BooksStore.Authors",
+                l["Menu:Authors"],
+                url: "/authors"
+            ));
+        }
+
     }
 
     private Task ConfigureUserMenuAsync(MenuConfigurationContext context)
