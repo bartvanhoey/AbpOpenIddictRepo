1d4ed94189a3b625cb5785a23c669042aabf367c
bartvanhoey <bartvanhoey@hotmail.com>
2024-07-09 09:50:33 +0200

    Update

--- BookStoreWebApi/BookStoreWebApi/Models/PagedResultDto.cs
+++ BookStoreWebApi/BookStoreWebApi/Models/PagedResultDto.cs
@@ -1,3 +1,18 @@
 ﻿namespace BookStoreWebApi.Models;
 
-public class PagedResultDto<T> : ListResultDto<T>, IPagedResult<T> {  }
+public class PagedResultDto<T> : ListResultDto<T>, IPagedResult<T>
+{
+
+      public long TotalCount { get; set; } 
+
+      public PagedResultDto()
+    {
+
+    }
+
+    public PagedResultDto(long totalCount, IReadOnlyList<T> items) : base(items)
+    {
+        TotalCount = totalCount;
+    }
+    
+}
